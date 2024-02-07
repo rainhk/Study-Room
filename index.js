1581cf5b32fbcb6ad27e62c4b3b8f6643e6de7d6
Doyeon Kim <doyeonkim.2012@gmail.com>
2024-02-06 21:55:14 -0500

    Player move
    Player moves when key pressed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+
 canvas.width = 1024
 canvas.height = 576
 
@@ -13,27 +14,113 @@ mapImage.src = './image/room map_zoomed.png'
 const playerImage = new Image()
 playerImage.src = './image/playerDown.png'
 
-mapImage.onload = () => {
-    c.drawImage(mapImage,-600,-200)
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position: {
+    x: -600,
+    y: -200
+    },
+    image: mapImage
+})
+
+const keys = {
+    w: {
+        pressed : false
+    },
+    a: {
+        pressed : false
+    },
+    s: {
+        pressed : false
+    },
+    d: {
+        pressed : false
+    }
+}
+
+function keyPressed(){
+    if (keys.w.pressed) {
+        background.position.y += 3
+    }
+    else if (keys.s.pressed) {
+        background.position.y -= 3
+    }
+    else if (keys.d.pressed) {
+        background.position.x -= 3
+    }
+    else if (keys.a.pressed) {
+        background.position.x += 3
+    }
+}
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(playerImage,
         0,0,playerImage.width/4,playerImage.height, 
         (canvas.width - (playerImage.width/4)) / 2 , 
         (canvas.height - playerImage.height) / 2,
         playerImage.width/4,playerImage.height)
+
+    keyPressed()
+    // if (keys.w.pressed) {
+    //     background.position.y += 3
+    // }
+    // else if (keys.w.pressed) {
+    //     background.position.y += 3
+    // }
+    // else if (keys.w.pressed) {
+    //     background.position.y += 3
+    // }
+    // else if (keys.w.pressed) {
+    //     background.position.y += 3
+    // }
 }
 
+animate()
+
 window.addEventListener('keydown', (e) => {
-    console.log(e.key)
     switch (e.key) {
         case 'w':
+            keys.w.pressed = true
             break
         case 'a':
+            keys.a.pressed = true
             break
         case 's':
+            keys.s.pressed = true
             break
         case 'd':
+            keys.d.pressed = true
             break
         
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+        case 's':
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
     }
 })
 
